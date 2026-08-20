6dc2248a53e736eeaaec905300f7623181116d0c
Frostwrath99 <gabryjlau@gmail.com>
2026-08-20 23:50:04 +0800

    Edit greeting

--- src/main/java/Es.java
+++ src/main/java/Es.java
@@ -2,12 +2,21 @@
  * Entry point for the Es chatbot application.
  */
 public class Es {
+    private static final String DIVIDER = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = " _____     \n"
                 + "| ____|___ \n"
                 + "|  _| / __|\n"
                 + "| |___\\__ \\\n"
                 + "|_____|___/\n";
-        System.out.println(banner);
+
+        System.out.println(DIVIDER);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Es.");
+        System.out.println("What can I do for you?");
+        System.out.println(DIVIDER);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
     }
 }
