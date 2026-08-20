5c62f38b738b02b2ad738805f76548dd5752760a
Frostwrath99 <gabryjlau@gmail.com>
2026-08-20 18:07:36 +0800

    Renaming the chatbot

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Es User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Es.java
@@ -0,0 +1,13 @@
+/**
+ * Entry point for the Es chatbot application.
+ */
+public class Es {
+    public static void main(String[] args) {
+        String banner = " _____     \n"
+                + "| ____|___ \n"
+                + "|  _| / __|\n"
+                + "| |___\\__ \\\n"
+                + "|_____|___/\n";
+        System.out.println(banner);
+    }
+}
