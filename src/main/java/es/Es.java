be47cce9a4416e5be800e9785426f193df8ea22f
Frostwrath99 <gabryjlau@gmail.com>
2026-09-15 19:53:30 +0800

    Mark unknown input as an error
    Use the reserved ellipsis wording for unknown commands in every execution path so the GUI error styling and console response agree.

--- src/main/java/es/Es.java
+++ src/main/java/es/Es.java
@@ -23,7 +23,7 @@ public class Es {
         Command command = Parser.parse(commandText);
         try {
             if (command == null) {
-                throw new EsException("I didn't expect you to say that.");
+                throw new EsException("... I didn't expect you to say that.");
             }
             switch (command) {
             case TODO:
@@ -200,7 +200,7 @@ public class Es {
                     if (input.isEmpty()) {
                         throw new EsException("You are silent. Give me something to work with.");
                     }
-                        throw new EsException("I didn't expect you to say that.");
+                        throw new EsException("... I didn't expect you to say that.");
                 }
 
                 switch (command) {
@@ -238,7 +238,7 @@ public class Es {
                     addEvent(storage, tasks, input.substring(command.name().length()).trim());
                     break;
                 default:
-                    throw new EsException("I didn't expect you to say that.");
+                    throw new EsException("... I didn't expect you to say that.");
                 }
             } catch (EsException e) {
                 ui.showError(e.getMessage());
